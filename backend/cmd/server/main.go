bd5eb1191d5cd6e4c3a4d4624e84733f8d73b3be
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-05-23 05:53:30 +0700

    UPDATE: Added added server initialization

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"github.com/AlvinSetyaPranata/ZENITH/backend/config"
+)
+
 func main() {
+	app := config.CreateFiber()
+	// db := config.NewDatabase()
+
+	err := app.Listen(":8000")
+
+	if err != nil {
+		config.Log.Critical("Failed to initialize server: ", err.Error())
+	}
 
 }
