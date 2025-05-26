481dc9dc45e90c7cd0f92d9e4cd5ad26ab5b95f7
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-05-26 09:29:39 +0700

    UPDATE: Changed from patch to using put instead

--- backend/api/routes/route.go
+++ backend/api/routes/route.go
@@ -17,13 +17,13 @@ func (r *RouteConfig) SetupRoute() {
 	r.App.Get("/api/cities", r.CityHandler.Get)
 	r.App.Get("/api/city/:id", r.CityHandler.GetById)
 	r.App.Post("/api/city", r.CityHandler.Create)
-	r.App.Patch("/api/city/:id", r.CityHandler.Update)
+	r.App.Put("/api/city/:id", r.CityHandler.Update)
 	r.App.Delete("/api/city/:id", r.CityHandler.Delete)
 
 	// Religion routes
 	r.App.Get("/api/religions", r.ReligionHandler.GetAll)
 	r.App.Get("/api/religion/:id", r.ReligionHandler.GetByID)
 	r.App.Post("/api/religion", r.ReligionHandler.Create)
-	r.App.Patch("/api/religion/:id", r.ReligionHandler.Update)
+	r.App.Put("/api/religion/:id", r.ReligionHandler.Update)
 	r.App.Delete("/api/religion/:id", r.ReligionHandler.Delete)
 }
