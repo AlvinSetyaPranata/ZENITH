5a17d683827d7481f04f31f590a64f6a5c2c1166
mfhaisall <mhfaisal011@gmail.com>
2025-05-31 15:26:05 +0700

    first

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -2,7 +2,7 @@ import { useLocation } from "@solidjs/router";
 
 export default function Nav() {
   const location = useLocation();
-  const active = (path: string) =>
+  const active = (path) =>
     path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
   return (
     <nav class="bg-sky-800">
