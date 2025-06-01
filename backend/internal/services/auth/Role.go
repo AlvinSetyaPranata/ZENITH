e4076b7adc0a20571f1af5aa61be46c301db4880
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-06-01 16:37:01 +0700

    UPDATE: Added permission repository to the role service for accessing permission enitity

--- backend/internal/services/auth/Role.go
+++ backend/internal/services/auth/Role.go
@@ -10,37 +10,56 @@ import (
 )
 
 type RoleService struct {
-	RoleRepository *repositories.RoleRepository
-	Log            *zap.SugaredLogger
+	RoleRepository       *repositories.RoleRepository
+	PermissionRepository *repositories.PermissionRepository
+	Log                  *zap.SugaredLogger
 }
 
-func NewRoleService(roleRepository *repositories.RoleRepository, log *zap.SugaredLogger) *RoleService {
+func NewRoleService(roleRepository *repositories.RoleRepository, permissionRepository *repositories.PermissionRepository, log *zap.SugaredLogger) *RoleService {
 	return &RoleService{
-		RoleRepository: roleRepository,
-		Log:            log,
+		RoleRepository:       roleRepository,
+		PermissionRepository: permissionRepository,
+		Log:                  log,
 	}
 }
 
 // Core services
 
-func (Service *RoleService) CreateNewRoleData(ctx *fiber.Ctx, roleRequestModel *models.RoleModelRequest) (*entities.Role, int, string) {
+func (Service *RoleService) CreateNewRoleData(ctx *fiber.Ctx, roleRequestModel *models.RoleModelRequest) (*entities.Role, *[]entities.Permission, int, string) {
 
 	if err := ctx.BodyParser(roleRequestModel); err != nil {
-		return nil, 400, "Invalid Request"
+		return nil, nil, 400, "Invalid Request"
 	}
 
-	roleEntity := new(entities.Role)
+	// get permission
+
+	permissionEntities := make([]entities.Permission, 0, len(roleRequestModel.Permissions))
+
+	permissionEntity := new(entities.Permission)
+
+	for _, permissionName := range roleRequestModel.Permissions {
+		if err := Service.PermissionRepository.GetByName(ctx.UserContext(), permissionEntity, permissionName); err != nil {
+			return nil, nil, 404, err.Error()
+		}
+		permissionEntities = append(permissionEntities, *permissionEntity)
+		permissionEntity = new(entities.Permission)
+	}
+
+	roleEntity := &entities.Role{
+		Name:        roleRequestModel.Name,
+		Permissions: permissionEntities,
+	}
 
 	if err := Service.RoleRepository.Create(ctx.UserContext(), roleEntity); err != nil {
 
 		if errType := utils.HandlerError(err); errType == fiber.StatusConflict {
-			return nil, 409, err.Error()
+			return nil, nil, 409, err.Error()
 		}
 
-		return nil, 500, err.Error()
+		return nil, nil, 500, err.Error()
 	}
 
-	return roleEntity, 201, "New role has already created, successfully!"
+	return roleEntity, &permissionEntities, 201, "New role has already created, successfully!"
 }
 
 func (Service *RoleService) GetAllRolesData(ctx *fiber.Ctx) (*[]entities.Role, int, string) {
@@ -72,52 +91,63 @@ func (Service *RoleService) GetRoleById(ctx *fiber.Ctx) (*entities.Role, int, st
 	return roleEntity, 200, "Role data with given, ID"
 }
 
-func (Service *RoleService) UpdateRoleData(ctx *fiber.Ctx, newRoleDataRequest *models.RoleModelRequest) (*entities.Role, int, string) {
+func (Service *RoleService) UpdateRoleData(ctx *fiber.Ctx, newRoleDataRequest *models.RoleModelRequest) (*entities.Role, *[]entities.Permission, int, string) {
 
 	id := ctx.Params("id", "")
 
 	if id == "" {
-		return nil, 400, "Invalid Request!"
+		return nil, nil, 400, "Invalid Request!"
 	}
 
 	if err := ctx.BodyParser(newRoleDataRequest); err != nil {
-		return nil, 400, "Invalid Request!"
+		return nil, nil, 400, "Invalid Request!"
+	}
+
+	// Set the permissions
+
+	currentPermissions := make([]entities.Permission, 0, len(newRoleDataRequest.Permissions))
+
+	currentPermission := new(entities.Permission)
+
+	for _, permissionName := range newRoleDataRequest.Permissions {
+		if err := Service.PermissionRepository.GetByName(ctx.UserContext(), currentPermission, permissionName); err != nil {
+			return nil, nil, 404, err.Error()
+		}
+
+		currentPermissions = append(currentPermissions, *currentPermission)
+		currentPermission = new(entities.Permission)
 	}
 
 	currentEntity := new(entities.Role)
 
 	if err := Service.RoleRepository.GetRoleById(ctx.UserContext(), currentEntity, id); err != nil {
 		if errType := utils.HandlerError(err); errType == fiber.StatusNotFound {
-			return nil, 404, "No Role data with given id!"
+			return nil, nil, 404, "No Role data with given id!"
 		}
 
-		return nil, 500, err.Error()
+		return nil, nil, 500, err.Error()
 	}
 
 	newRoleEntity := &entities.Role{
 		Id:          currentEntity.Id,
 		Name:        newRoleDataRequest.Name,
-		Permissions: nil,
+		Permissions: currentPermissions,
 		DateCreated: currentEntity.DateCreated,
 	}
 
 	if err := Service.RoleRepository.Update(ctx.UserContext(), newRoleEntity, id); err != nil {
-		return nil, 500, err.Error()
+		return nil, nil, 500, err.Error()
 	}
 
-	return newRoleEntity, 200, "New role data with given id, has been updated successfully!"
+	return newRoleEntity, &currentPermissions, 200, "New role data with given id, has been updated successfully!"
 
 }
 
-func (Service *RoleService) DeleteRoleData(ctx *fiber.Ctx, newRoleDataRequest *models.RoleModelRequest) (int, string) {
+func (Service *RoleService) DeleteRoleData(ctx *fiber.Ctx) (int, string) {
 
 	id := ctx.Params("id", "")
 
 	if id == "" {
-		return 400, "Invalid Request!"
-	}
-
-	if err := ctx.BodyParser(newRoleDataRequest); err != nil {
 		return 400, "Invalid Request!"
 	}
 
@@ -131,14 +161,7 @@ func (Service *RoleService) DeleteRoleData(ctx *fiber.Ctx, newRoleDataRequest *m
 		return 500, err.Error()
 	}
 
-	newRoleEntity := &entities.Role{
-		Id:          currentEntity.Id,
-		Name:        newRoleDataRequest.Name,
-		Permissions: nil,
-		DateCreated: currentEntity.DateCreated,
-	}
-
-	if err := Service.RoleRepository.Delete(ctx.UserContext(), newRoleEntity, id); err != nil {
+	if err := Service.RoleRepository.Delete(ctx.UserContext(), currentEntity, id); err != nil {
 		return 500, err.Error()
 	}
 
