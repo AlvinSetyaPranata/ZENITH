70ac8a7fd82b9bddcc0a13c70876945fbe727338
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-06-03 06:19:38 +0700

    ADD: Added authorization middlewere

---
+++ backend/api/middlewares/Authorization.go
@@ -0,0 +1,59 @@
+package middlewares
+
+import (
+	"strings"
+
+	"slices"
+
+	utils "github.com/AlvinSetyaPranata/ZENITH/backend/utils/auth"
+	"github.com/gofiber/fiber/v2"
+)
+
+func AuthenticationCheck(c *fiber.Ctx) error {
+	tokenHeader := c.Get("Authorization")
+
+	if tokenHeader == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"messege": "Unauthorized, You have no access rights!",
+		})
+	}
+
+	splitedTokenHeader := strings.Split(tokenHeader, " ")
+
+	if len(splitedTokenHeader) != 2 || splitedTokenHeader[0] != "Bearer" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"messege": "Unauthorized, You have no access rights!",
+		})
+	}
+
+	token := splitedTokenHeader[1]
+
+	userID, role, isValid := utils.ParseToken(token, true)
+
+	if isValid != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"messege": "Unauthorized, You have no access rights!",
+		})
+	}
+
+	c.Locals("userID", userID)
+	c.Locals("role", role)
+
+	return c.Next()
+}
+
+func RoleGuard(allowedRoles ...string) fiber.Handler {
+
+	return func(c *fiber.Ctx) error {
+		role := c.Locals("role").(string)
+
+		if slices.Contains(allowedRoles, role) {
+			return c.Next()
+		}
+
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"messege": "You, forbids to access this resources",
+		})
+	}
+
+}
