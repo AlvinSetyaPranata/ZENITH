6f0c40da2211daed5330874908e77c41c6691c76
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-07-15 06:43:21 +0700

    UPDATE: Added CORS for local frontend development

--- backend/internal/config/fiber.go
+++ backend/internal/config/fiber.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"go.uber.org/zap"
 )
 
@@ -10,6 +11,14 @@ func CreateFiber(log *zap.SugaredLogger) *fiber.App {
 	app := fiber.New(fiber.Config{
 		DisableStartupMessage: true,
 	})
+
+	cors_config := cors.Config{
+		AllowOrigins: "http://127.0.0.1:3000,http://localhost:3000",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
+	}
+
+	app.Use(cors.New(cors_config))
 
 	log.Debug("Fiber succesfully configured")
 
