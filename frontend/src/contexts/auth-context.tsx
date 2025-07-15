f7b0109baa38481cb93e94754bea04831236c490
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-07-15 09:21:29 +0700

    UPDATE: Added handler to get token if not then it will parse from session storage

--- frontend/src/contexts/auth-context.tsx
+++ frontend/src/contexts/auth-context.tsx
@@ -3,44 +3,79 @@ import { createStore } from "solid-js/store";
 import { AuthContextType, AuthStoreType } from "~/types/auth-types/store";
 import { LoginWithNimService } from "~/services/auth/login";
 import { LoginWithNimCredentialsType } from "~/types/auth-types/credentials";
-
-
+import { UserType } from "~/types/master-types/role-type";
 
 export const AuthContext = createContext<AuthContextType>();
 
-
 export function AuthProvider(props: ParentProps) {
-    const [user, setUser] = createStore<AuthStoreType>({
-        user: null,
-    })
-    
-    const login = async (credentials: LoginWithNimCredentialsType) => {
-        const response = await LoginWithNimService(credentials)
+  const [user, setUser] = createStore<AuthStoreType>({
+    user: null,
+    token: null,
+  });
 
-        if (response.status == 401) {
-            // TODO: Refetch to update access token
-            console.log("Unauthenticated")
-            return false
-        }
+  const storeUserData = (user: UserType, token: string) => {
+    // this function should be run before profile data is stored in memory
+    // for persistent storage we use session storage with value that has been obfuscate before
 
-        const data = await response.json()
-        
-        setUser({
-            user: data.data,
-            token: data.access_token
-        })
-        return true
-        
+    if (user) {
+      // TODO: Add obfuscate method to perform
+      sessionStorage.setItem("user", JSON.stringify(user));
     }
 
-    const contextPayload: AuthContextType = {
-        auth: user,
-        login: login
+    if (token) {
+      // TODO: Add obfuscate method to perform
+      sessionStorage.setItem("token", token);
     }
 
-    return (
-        <AuthContext.Provider value={contextPayload}>
-            {props.children}
-        </AuthContext.Provider>
-    )   
+    setUser({
+      user: user,
+      token: token,
+    });
+  };
+
+  const getUserData = () => {
+    if (user.user) return user;
+
+    const userData = sessionStorage.getItem("user");
+    const token = sessionStorage.getItem("token");
+
+    // Profile data in memory is missing due to page refresh or hot reload
+    if (!userData) return null;
+
+    // TODO: Add deobfuscate method to extract user profile to inserted in memory
+
+    const parsedUserData = JSON.parse(userData)
+
+    setUser({ user: parsedUserData, token: token });
+
+    return parsedUserData
+  };
+
+  const login = async (credentials: LoginWithNimCredentialsType) => {
+    const response = await LoginWithNimService(credentials);
+
+    if (response.status == 401) {
+      // TODO: Refetch to update access token
+      console.log("Unauthenticated");
+      return false;
+    }
+
+    const data = await response.json();
+
+    storeUserData(data.data, data.access_token);
+    return true;
+  };
+
+  const contextPayload: AuthContextType = {
+    auth: user,
+    login: login,
+    storeUserData: storeUserData,
+    getUserData: getUserData,
+  };
+
+  return (
+    <AuthContext.Provider value={contextPayload}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 }
--- frontend/src/types/auth-types/store.ts
+++ frontend/src/types/auth-types/store.ts
@@ -5,12 +5,14 @@ import { LoginWithNimCredentialsType } from "./credentials";
 
 export interface AuthStoreType {
     user: UserType | null;
-    token?: string;
+    token: string|null;
 }
 
 
 export interface AuthContextType {
     auth: AuthStoreType;
     login: (credentials: LoginWithNimCredentialsType) => Promise<boolean>;
+    storeUserData: (user: UserType, token: string) => void;
+    getUserData: () => AuthStoreType|null|undefined
 }
 
