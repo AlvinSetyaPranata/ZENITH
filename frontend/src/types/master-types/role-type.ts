3c11e8d9be67d38a9b2716ea355daf9f4794535b
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-07-15 09:21:50 +0700

    UPDATE: Include role type inside user type

--- frontend/src/types/master-types/role-type.ts
+++ frontend/src/types/master-types/role-type.ts
@@ -1,12 +1,13 @@
 
-export interface UserType {
-    email: string;
-    password: string;
-}
 
 export interface RoleType {
     Id: number;
     Name: string;
     Permissions: [] | null;
     DateCreated: string
+}
+export interface UserType {
+    id: number;
+    email: string;
+    role: RoleType
 }
