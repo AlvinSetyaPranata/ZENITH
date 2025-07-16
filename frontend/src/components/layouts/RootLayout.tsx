e6cddc85f647eb576a089b79755a2bed2dd4a0ca
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-07-16 22:29:49 +0700

    UPDATE: Dynamiclly get profile name and role

--- frontend/src/components/layouts/RootLayout.tsx
+++ frontend/src/components/layouts/RootLayout.tsx
@@ -1,16 +1,46 @@
-import { createEffect, createSignal, ParentProps } from "solid-js";
+import {
+  createEffect,
+  createResource,
+  createSignal,
+  onMount,
+  ParentProps,
+  Show,
+  useContext,
+} from "solid-js";
 import Sidebar from "../ui/sidebar";
 import SidebarNav from "../molecules/sidebar-nav";
-import { A } from "@solidjs/router";
+import { A, useNavigate } from "@solidjs/router";
 import { Icon } from "@iconify-icon/solid";
+import { AuthContext } from "~/contexts/auth-context";
+import { UserType } from "~/types/master-types/role-type";
+import { AuthStoreType } from "~/types/auth-types/store";
+import { GetStudentData } from "~/services/api/students/get-student-profile";
 
 export default function RootLayout({ children }: ParentProps) {
+  const navigate = useNavigate();
   const [activeNav, setActiveNav] = createSignal(1);
   const [sidebarOpened, setSidebarOpened] = createSignal(true);
-  const [profilePopover, setProfilePopover] = createSignal(false)
+  const [profilePopover, setProfilePopover] = createSignal(false);
 
+  const [data, { refetch }] = createResource(GetStudentData);
 
-  // createEffect(() => console.log(sidebarOpened()), [sidebarOpened])
+  createEffect(() => {
+    if (data.error) {
+      navigate("/");
+    }
+  }, [data.error]);
+
+  createEffect(() => {
+    if (data() == false) {
+      navigate("/");
+    }
+  }, [data]);
+
+  onMount(() => {
+    refetch();
+  });
+
+  createEffect(() => console.log(data()), [data]);
 
   return (
     <div class="flex bg-white min-h-screen w-full">
@@ -61,12 +91,29 @@ export default function RootLayout({ children }: ParentProps) {
           {/* profile section */}
           <div class="flex items-center gap-x-4 relative">
             <div class="space-y-1 flex flex-col items-end">
-              <h4 class="font-medium">Alvin Setya Pranata</h4>
-              <span class="text-sm text-gray-400">Mahasiswa</span>
+              <Show
+                when={data()}
+                fallback={
+                  <div class="h-[10px] w-[100px] bg-gray-600 animate-pulse "></div>
+                }
+              >
+                <h4 class="font-medium">{data().name}</h4>
+              </Show>
+              <Show
+                when={data()}
+                fallback={
+                  <div class="h-[10px] w-[100px] bg-gray-600 animate-pulse "></div>
+                }
+              >
+                <span class="text-sm text-gray-400 capitalize">{data().user.role_name}</span>
+              </Show>
             </div>
 
             {/* photo section */}
-            <button on:click={() => setProfilePopover((prev) => !prev)} class="flex gap-x-2 items-center hover:cursor-pointer">
+            <button
+              on:click={() => setProfilePopover((prev) => !prev)}
+              class="flex gap-x-2 items-center hover:cursor-pointer"
+            >
               <div class="size-[50px] rounded-full overflow-hidden">
                 <img
                   src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -81,15 +128,29 @@ export default function RootLayout({ children }: ParentProps) {
             </button>
             {/* photo section */}
             {profilePopover() && (
-              <button on:click={() => setProfilePopover(false)} class="fixed min-w-screen min-h-screen bg-black/0 z-20 top-0 left-0"></button>
+              <button
+                on:click={() => setProfilePopover(false)}
+                class="fixed min-w-screen min-h-screen bg-black/0 z-20 top-0 left-0"
+              ></button>
             )}
 
-
             {/* profile popover section */}
-            <div class={`absolute z-30 right-0 -bottom-36 w-[250px] rounded-md bg-white p-4 text-center space-y-4 ${profilePopover() ? 'opacity-100 transition-opacity duration-300 ease-out' : 'opacity-0 transition-opacity duration-300 ease-in'}`}>
-              <A href="/" class="text-black block hover:underline">Akun anda</A>
-              <A href="/" class="text-black block hover:underline">Pengaturan</A>
-              <A href="/" class="text-red-500 block hover:underline">Keluar</A>
+            <div
+              class={`absolute z-30 right-0 -bottom-36 w-[250px] rounded-md bg-white p-4 text-center space-y-4 ${
+                profilePopover()
+                  ? "opacity-100 transition-opacity duration-300 ease-out"
+                  : "opacity-0 transition-opacity duration-300 ease-in"
+              }`}
+            >
+              <A href="/" class="text-black block hover:underline">
+                Akun anda
+              </A>
+              <A href="/" class="text-black block hover:underline">
+                Pengaturan
+              </A>
+              <A href="/" class="text-red-500 block hover:underline">
+                Keluar
+              </A>
             </div>
             {/* profile popover section */}
           </div>
