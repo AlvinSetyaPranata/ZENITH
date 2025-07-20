35c46e407c4ae047d8f9840088adaf0ddd85770c
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-07-20 22:18:14 +0700

    CLEANING

--- frontend/src/components/molecules/sonner-group.tsx
+++ frontend/src/components/molecules/sonner-group.tsx
@@ -26,21 +26,16 @@ export default function SonnerGroup(props: SoonerGroupPropsType) {
   const onSonnerFinished = (id: number) => {
     if (props.soonersData().length == 1) {
       setTimeout(() => {
-        // console.log("ID: " + id + ", was passed to the on finished");
-        // console.log(props.soonersData());
         props.soonersSetter((prevs) =>
           prevs.map((item) =>
             item.id === id ? { ...item, isRemoved: true } : item
           )
         );
-        // setVisible((prev) => prev.filter((item) => item.id !== id))
       }, 900);
 
       return;
     }
 
-    // console.log("ID: " + id + ", was passed to the on finished")
-    // console.log(props.soonersData())
     props.soonersSetter((prevs) =>
       prevs.map((item) =>
         item.id === id ? { ...item, isRemoved: true } : item
@@ -48,27 +43,24 @@ export default function SonnerGroup(props: SoonerGroupPropsType) {
     );
   };
 
-  createEffect(() => console.log(props.soonersData()), [props.soonersData])
+  createEffect(() => console.log(props.soonersData()), [props.soonersData]);
 
   return (
     <div class="fixed top-0 left-1/2 -translate-x-1/2 overflow-visible flex flex-col gap-y-8 pt-8 h-max">
       <For
         each={props.soonersData()}
-        children={(item) => {
-          // console.log("ID: " + item.id + ", with status: " + item.isRemoved)
-          return (
-            <Presence>
-              <Show keyed when={!item.isRemoved}>
-                <Sooner
-                  id={item.id}
-                  title={item.title}
-                  desc={item.desc}
-                  setIsClosed={onSonnerFinished}
-                />
-              </Show>
-            </Presence>
-          );
-        }}
+        children={(item) => (
+          <Presence>
+            <Show keyed when={!item.isRemoved}>
+              <Sooner
+                id={item.id}
+                title={item.title}
+                desc={item.desc}
+                setIsClosed={onSonnerFinished}
+              />
+            </Show>
+          </Presence>
+        )}
       />
     </div>
   );
